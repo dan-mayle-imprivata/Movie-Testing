75a06e605a679d5182d8d6e4c7135d3d57a4b9ef
SCDan0624 <dmayle1@hotmail.com>
2020-11-03 15:51:10 -0800

    added chai assertions

--- test/autocomplete.test.js
+++ test/autocomplete.test.js
@@ -1,1 +1,19 @@
-it("works!!", () => {});
+it("Shows an autocomplete", () => {
+  createAutoComplete({
+    root: document.querySelector("#target"),
+    fetchData() {
+      return [
+        { Title: "Avengers" },
+        { Title: "Not Avengers" },
+        { Title: "Some other movie" },
+      ];
+    },
+    renderOption(movie) {
+      return movie.Title;
+    },
+  });
+
+  const dropdown = document.querySelector(".dropdown");
+
+  expect(dropdown.className).not.to.include("is-active");
+});
